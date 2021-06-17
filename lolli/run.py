253708b8e4b3033fda30f85cff83ecc97510ad01
maccam912 <maccam912@gmail.com>
2021-06-17 14:46:27 -0500

    switching to backtesting package

--- lolli/run.py
+++ lolli/run.py
@@ -1,57 +1,50 @@
-from __future__ import absolute_import, division, print_function, unicode_literals
-
 import os
-import sys
-from datetime import datetime
-from pathlib import Path
 
-import backtrader as bt
+import pandas as pd
+import tda
 import typer
+from backtesting import Backtest
+from tda.auth import easy_client
+from tda.client import Client
 
-from lolli.strategy import TestStrategy
+from lolli.strategy import SmaCross
 
 app = typer.Typer()
 
-
-def datafile(path: str, fromdate: datetime, todate: datetime):
-    modpath = os.path.dirname(os.path.abspath(sys.argv[0]))
-    datapath = os.path.join(modpath, path)
-
-    data = bt.feeds.YahooFinanceCSVData(
-        dataname=datapath, fromdate=fromdate, todate=todate, reverse=False
-    )
-
-    return data
+token_path = os.path.join(os.getenv("HOME"), ".config", "tdtoken")
 
 
 @app.command()
-def run(
-    path: Path,
-    fromdate: datetime = typer.Argument(None, formats=["%Y-%m-%d"]),
-    todate: datetime = typer.Argument(None, formats=["%Y-%m-%d"]),
-):
-    cerebro = bt.Cerebro()
+def login():
+    tda.auth.client_from_manual_flow(
+        api_key=os.getenv("TDA_API_KEY"),
+        redirect_url="https://127.0.0.1:4000",
+        token_path=token_path,
+    )
 
-    # Add a strategy
-    # strats = cerebro.optstrategy(TestStrategy, maperiod=range(1, 31))
-    strats = cerebro.addstrategy(TestStrategy, maperiod=21)
 
-    # Add the Data Feed to Cerebro
-    cerebro.adddata(datafile(path, fromdate, todate))
+@app.command()
+def run(symbol: str):
+    c = easy_client(
+        api_key=os.getenv("TDA_API_KEY"),
+        redirect_uri="https://127.0.0.1:4000",
+        token_path=token_path,
+    )
+    hist = c.get_price_history(
+        symbol,
+        period_type=Client.PriceHistory.PeriodType.DAY,
+        period=Client.PriceHistory.Period.FIVE_YEARS,
+        frequency_type=Client.PriceHistory.FrequencyType.MINUTE,
+        frequency=Client.PriceHistory.Frequency.EVERY_MINUTE,
+    )
+    pd_hist = pd.DataFrame(hist.json()["candles"])
+    print(pd_hist)
 
-    # Set our desired cash start
-    cerebro.broker.setcash(100000.0)
-
-    # Add a FixedSize sizer according to the stake
-    cerebro.addsizer(bt.sizers.FixedSize, stake=10)
-
-    # Set the commission
-    cerebro.broker.setcommission(commission=0.0)
-
-    # Run over everything
-    cerebro.run(maxcpus=1)
-
-    cerebro.plot()
+    bt = Backtest(
+        pd_hist, SmaCross, cash=10000, commission=0.002, exclusive_orders=True
+    )
+    output = bt.run()
+    bt.plot()
 
 
 def main():
--- lolli/strategy.py
+++ lolli/strategy.py
@@ -1,62 +1,19 @@
-import backtrader as bt
-from backtrader.indicators.crossover import CrossDown, CrossUp
+from backtesting import Strategy
+from backtesting.lib import crossover
+from backtesting.test import SMA
 
 
-class TestStrategy(bt.Strategy):
-    params = (
-        ("maperiod", 13),
-        ("printlog", False),
-    )
+class SmaCross(Strategy):
+    n1 = 10
+    n2 = 20
 
-    def log(self, txt, dt=None, doprint=False):
-        """Logging function fot this strategy"""
-        if self.params.printlog or doprint:
-            dt = dt or self.datas[0].datetime.date(0)
-            print("%s, %s" % (dt.isoformat(), txt))
-
-    def __init__(self):
-        # Keep a reference to the "close" line in the data[0] dataseries
-        self.dataclose = self.datas[0].close
-        self.started = False
-
-        # To keep track of pending orders and buy price/commission
-        self.order = None
-        self.buyprice = None
-        self.buycomm = None
-
-        # Add a MovingAverageSimple indicator
-        self.sma1 = bt.indicators.SimpleMovingAverage(
-            self.datas[0], period=self.params.maperiod
-        )
-
-        self.sma2 = bt.indicators.SimpleMovingAverage(
-            self.datas[0], period=(self.params.maperiod) * 2
-        )
-
-    def notify_trade(self, trade):
-        if not trade.isclosed:
-            return
-
-        self.log("OPERATION PROFIT, GROSS %.2f, NET %.2f" % (trade.pnl, trade.pnlcomm))
+    def init(self):
+        close = self.data.Close
+        self.sma1 = self.I(SMA, close, self.n1)
+        self.sma2 = self.I(SMA, close, self.n2)
 
     def next(self):
-        if not self.started:
-            if self.sma1[0] > self.sma2[0]:
-                self.buy()
-            else:
-                self.sell()
+        if crossover(self.sma1, self.sma2):
             self.buy()
-            self.started = True
-        if self.sma1[-1] <= self.sma2[-1] and self.sma1[0] > self.sma2[0]:
-            self.buy(size=2)
-        if self.sma1[-1] > self.sma2[-1] and self.sma1[0] <= self.sma2[0]:
-            self.sell(size=2)
-
-    def stop(self):
-        self.close()
-
-        self.log(
-            "(MA Period %2d) Ending Value %.2f"
-            % (self.params.maperiod, self.broker.getvalue()),
-            doprint=True,
-        )
+        elif crossover(self.sma2, self.sma1):
+            self.sell()
